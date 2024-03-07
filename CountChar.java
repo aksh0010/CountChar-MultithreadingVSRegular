3f5497b6c11197a187b25b345580335555e6abb8
aksh0010 <akshpatelofficial@gmail>
2024-03-07 14:18:51 -0500

    confing java home on vscode

--- CountChar.java
+++ CountChar.java
@@ -4,20 +4,31 @@ import java.util.Scanner;
 
 public class CountChar {
 
-  private static Scanner input;
+  private static Scanner input_Scanner;
   private static File file;
 
   public static void main(String[] args) {
-    input = new Scanner(System.in);
+    input_Scanner = new Scanner(System.in);
     file = new File("input.txt");
-
+    
     try {
-      input = new Scanner(file);
+      input_Scanner = new Scanner(file);
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
 
+    // System.out.println(getString());
+
+
   }
 
+  public static String getString() {
+    StringBuilder Final_string = new StringBuilder();
+    String each_line;
+    while ((each_line = input_Scanner.nextLine()) != null) {
+      Final_string.append(each_line);
+    }
 
+    return Final_string.toString();
+  }
 }

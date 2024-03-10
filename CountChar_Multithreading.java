2c346b3160ca6a099ce5b2d583c03cb7f4dcf8e6
aksh0010 <akshpatelofficial@gmail>
2024-03-10 17:49:55 -0400

    reverting Multithread to 7ecf9d3a52ead0115639f82d446c3107b03d165c

--- CountChar_Multithreading.java
+++ CountChar_Multithreading.java
@@ -1,113 +1,54 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class CountChar_Multithreading {
 
-    private Scanner inputScanner;
-    private File file;
-    private Lock lock;
-    private Condition condition;
-    private StringBuilder finalString;
-    private int whitespaceCount;
-    private boolean lineRead;
+    private static Scanner inputScanner = new Scanner(System.in);
+    private static File file = new File("input.txt");
+    private static StringBuilder finalString = new StringBuilder(); // shared string builder
+    private static int whitespaceCount = 0; // shared whitespace coun
+    
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        System.out.println("Multithread Main");
 
-    public CountChar_Multithreading(String filePath, int numThreads) {
-        file = new File(filePath);
-        lock = new ReentrantLock();
-        condition = lock.newCondition();
-        finalString = new StringBuilder();
-        whitespaceCount = 0;
-        lineRead = false;
         try {
             inputScanner = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-    }
 
-    public void countWhitespace() {
-        Thread readingThread = new Thread(() -> {
-            while (inputScanner.hasNextLine()) {
-                String line = inputScanner.nextLine();
-                lock.lock();
-                try {
-                    finalString.append(line);
-                    lineRead = true;
-                    condition.signal();
-                    condition.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    lock.unlock();
-                }
-            }
-            lock.lock();
-            try {
-                finalString.append('\0');
-                lineRead = true;
-                condition.signal();
-            } finally {
-                lock.unlock();
-            }
-        });
+        String content = getString();
+        countWhitespace(content);
 
-        Thread countingThread = new Thread(() -> {
-            int index = 0;
-            while (true) {
-                lock.lock();
-                try {
-                    while (!lineRead) {
-                        condition.await();
-                    }
-                    if (finalString.charAt(index) == '\0') {
-                        break;
-                    }
-                    int count = 0;
-                    while (index < finalString.length() && finalString.charAt(index) != '\n') {
-                        if (Character.isWhitespace(finalString.charAt(index))) {
-                            count++;
-                        }
-                        index++;
-                    }
-                    whitespaceCount += count;
-                    lineRead = false;
-                    condition.signal();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    lock.unlock();
-                }
-            }
-        });
-
-        readingThread.start();
-        countingThread.start();
-
-        try {
-            readingThread.join();
-            countingThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Total Char: " + (finalString.length()-1));
-        System.out.println("Total Whitespace: " + whitespaceCount);
-    }
-
-    public static void main(String[] args) {
-    	System.out.println("Main");
-    	String filePath = "input.txt";
-    	int numThreads = 2;
-    	CountChar_Multithreading counter = new CountChar_Multithreading(filePath, numThreads);
-    	    
-        long startTime = System.currentTimeMillis();
-        counter.countWhitespace();
+        System.out.println("Multithread Total Char: " + content.length());
+        System.out.println("Multithread Total Whitespace: " + whitespaceCount);
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Thread execution time: " + (endTime - startTime) + " milliseconds");
+        System.out.println("Multithread Thread execution time: " + (endTime - startTime) + " milliseconds");
+    }
+
+    public static String getString() {
+        System.out.println(" Multithread getString method");
+//        StringBuilder finalString = new StringBuilder();
+
+        while (inputScanner.hasNextLine()) {
+            String line = inputScanner.nextLine();
+            finalString.append(line);
+        }
+
+        return finalString.toString();
+    }
+
+    public static int countWhitespace(String str) {
+    	
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isWhitespace(str.charAt(i))) {
+            	whitespaceCount++;
+            }
+        }
+        return whitespaceCount;
     }
 }
